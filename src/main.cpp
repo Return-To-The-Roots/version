51a2fb674ebf06197a4dbbc5a9debef200fdd86e
Alexander Grund <Alex@grundis.de>
2015-07-27 19:03:00 +0200

    Convert src files to UTF-8

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// $Id: main.cpp 9361 2014-04-25 15:45:34Z FloSoft $
+﻿// $Id: main.cpp 9361 2014-04-25 15:45:34Z FloSoft $
 //
 // Copyright (c) 2005 - 2011 Settlers Freaks (sf-team at siedler25.org)
 //
