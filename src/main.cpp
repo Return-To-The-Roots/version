86e3d064df6022bdb73558fdc1ce96af09577bc1
Ulrich Spörlein <uspoerlein@gmail.com>
2015-08-07 22:39:29 +0200

    Remove Byte Order Markers (BOM)
    This fixes issue #165

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-﻿// $Id: main.cpp 9361 2014-04-25 15:45:34Z FloSoft $
+// $Id: main.cpp 9361 2014-04-25 15:45:34Z FloSoft $
 //
 // Copyright (c) 2005 - 2011 Settlers Freaks (sf-team at siedler25.org)
 //
